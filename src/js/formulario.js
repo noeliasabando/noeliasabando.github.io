879f4bd6d477f044c44240474ff1726bcf61bbfa
Sabando Gonzalez <lolia_15@hotmail.com>
2018-10-06 00:51:07 -0400

    limpio input

--- src/js/formulario.js
+++ src/js/formulario.js
@@ -2,12 +2,14 @@ function contactame() {
   let name = document.getElementById("name").value;
   let email = document.getElementById("email").value;
   let text = document.getElementById("text").value;
+  document.getElementById("name").value="";
+  document.getElementById("email").value="";
+  document.getElementById("text").value="";
   const data = {
     email: email,
     name: name,
     text: text,
   }
-
   fetch("https://us-central1-portafolio-e4f20.cloudfunctions.net/sendContactMail", {
     method: "POST",
     body: JSON.stringify(data), // body data type must match "Content-Type" header
